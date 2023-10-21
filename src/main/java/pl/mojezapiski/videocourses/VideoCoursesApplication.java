5e1a52c9f0b2e028ecca23516c85d31657a7a24e
Paweł Wilczek <5646246+beowoolf@users.noreply.github.com>
2023-10-21 09:15:51 +0200

    Project init

---
+++ src/main/java/pl/mojezapiski/videocourses/VideoCoursesApplication.java
@@ -0,0 +1,13 @@
+package pl.mojezapiski.videocourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoCoursesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/mojezapiski/videocourses/VideoCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mojezapiski.videocourses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoCoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
